563493a81eb4f37158c5985451b1f6ba8752994a
Daniel Rojas <rojasdaniel94@gmail.com>
2016-05-02 19:01:49 -0700

    Project Submission

---
+++ Comp440_Project/dbo/Stored Procedures/updateVersion.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[updateVersion]
+/*
+ Procedure that updates the product version. Takes a product title and version id. 
+*/
+@productTitle varchar(255) = NULL , @version varchar(255) = NULL 
+
+AS
+
+DECLARE @productID int; 
+
+SELECT @productID = id
+ FROM [dbo].[product] 
+ WHERE title= @productTitle;
+
+	BEGIN
+		UPDATE [dbo].[product]
+			SET version_id = @version
+			WHERE [title] = @productTitle;
+	END
